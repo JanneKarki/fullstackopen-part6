b2e27b27f744d1148d5d36632f00efa9b354aa9e
Janne Kärki <janne.karki@fmi.fi>
2026-09-05 14:22:49 +0300

    Tehtävä 6.9: anekdootit step8 tehty

--- anecdotes/src/store.js
+++ anecdotes/src/store.js
@@ -1,17 +1,23 @@
 
 import { create } from 'zustand'
+import anecdoteService from './services/anecdotes'
 
-const useAnecdoteStore = create((set) => ({
+const useAnecdoteStore = create((set, get) => ({
   anecdotes: [],
   filter: '',
   actions: {
-    vote: id => set(state => ({
-      anecdotes: state.anecdotes.map(anecdote =>
-        anecdote.id === id
-          ? { ...anecdote, votes: anecdote.votes + 1 }
-          : anecdote
+    vote: async id => {
+      const anecdote = get().anecdotes.find(anecdote => anecdote.id === id)
+      const updated = await anecdoteService.update(
+        id,
+        { ...anecdote, votes: anecdote.votes + 1 }
       )
-    })),
+      set(state => ({
+        anecdotes: state.anecdotes.map(anecdote =>
+          anecdote.id === id ? updated : anecdote
+        )
+      }))
+    },
     add: anecdote => set(state => ({
       anecdotes: state.anecdotes.concat(anecdote)
     })),
