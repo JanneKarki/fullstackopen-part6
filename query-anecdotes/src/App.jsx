9fcfd097ec64f3f587830c5672f21b0a1bf8c4c4
Janne Kärki <janne.karki@fmi.fi>
2026-09-05 16:11:18 +0300

    Tehtävä 6.19 custom hook tehty

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,37 +1,17 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { getAnecdotes, updateAnecdote } from './requests'
+import { useAnecdotes } from './hooks/useAnecdotes'
 
 const App = () => {
-  const queryClient = useQueryClient()
+  const { anecdotes, isPending, isError, vote } = useAnecdotes()
 
-  const updateAnecdoteMutation = useMutation({
-    mutationFn: updateAnecdote,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
-    }
-  })
-
-  const handleVote = (anecdote) => {
-    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
-  }
-
-  const result = useQuery({
-    queryKey: ['anecdotes'],
-    queryFn: getAnecdotes,
-    retry: 1,
-  })
-
-  if (result.isPending) {
+  if (isPending) {
     return <div>loading data...</div>
   }
 
-  if (result.isError) {
+  if (isError) {
     return <div>anecdote service not available due to problems in server</div>
   }
-
-  const anecdotes = result.data
 
   return (
     <div>
@@ -45,7 +25,7 @@ const App = () => {
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
+            <button onClick={() => vote(anecdote)}>vote</button>
           </div>
         </div>
       ))}
